ff9cc58f21b7732ede3ba0c04156ccb15e11745a
Nicholas Finch <ngf4@nau.edu>
2016-10-10 08:09:48 -0700

    Changes to be committed: 	modified:   .idea/misc.xml 	new file:   .idea/vcs.xml 	modified:   app/build.gradle 	new file:   app/src/main/java/com/tisaconundrum/snake/GameSettings.java 	new file:   app/src/main/res/anim/anim_for_classic_button.xml 	new file:   app/src/main/res/anim/anim_for_gamecenter_button.xml 	new file:   app/src/main/res/anim/anim_for_home_button.xml 	new file:   app/src/main/res/anim/anim_for_no_button.xml 	new file:   app/src/main/res/anim/anim_for_settings_button.xml 	new file:   app/src/main/res/anim/anim_for_title_left.xml 	new file:   app/src/main/res/anim/anim_for_title_middle.xml 	new file:   app/src/main/res/anim/anim_for_title_right.xml 	new file:   app/src/main/res/anim/back_anim_for_title_left.xml 	new file:   app/src/main/res/anim/back_anim_for_title_middle.xml 	new file:   app/src/main/res/anim/back_anim_for_title_right.xml 	new file:   app/src/main/res/anim/cycle_7.xml 	new file:   app/src/main/res/anim/fade_in.xml 	new file:   app/src/main/res/anim/fade_out.xml 	new file:   app/src/main/res/anim/reverse_for_classic_button.xml 	new file:   app/src/main/res/anim/reverse_for_gamecenter_button.xml 	new file:   app/src/main/res/anim/reverse_for_home_button.xml 	new file:   app/src/main/res/anim/reverse_for_no_button.xml 	new file:   app/src/main/res/anim/reverse_for_settings_button.xml 	new file:   app/src/main/res/anim/shake.xml

---
+++ app/src/main/java/com/tisaconundrum/snake/GameSettings.java
@@ -0,0 +1,31 @@
+package com.tisaconundrum.snake;
+
+/**
+ * Created by tisaconundrum on 10/10/2016.
+ */
+
+public final class GameSettings {
+    public static final int FOOD_POINTS = 3; // uppercase with underscores distinguishes from normal variables
+    public static final int NUMBER_POINTS = 20;
+    public static final int POINTS_BOMB_ANIMATION = 15;
+    public static final int POINTS_ANIMATION = 4;
+    public static final int POISON_FOOD = 5;
+    public static final String MY_AD_UNIT_ID = "TBA";
+    // flash animation for the app
+    public static final int ANIMATION_OPEN_BUTTON_DURATION = 400;
+    public static final int ANIMATION_CLOSE_BUTTON_DURATION = 400;
+    public static final int ANIMATION_SHOW_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_HIDE_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_SHOW_TITLE_DURATION = 400;
+    public static final int ANIMATION_HIDE_TITLE_DURATION = 400;
+
+    public static final int SHAKE_DURATION = 500;
+
+    public static final int GAME_THREAD = 80;
+
+    public static final int GAME_THREAD_BOMB = 60;
+
+    public static final int SWIPE_THRESH_HOLD = 100;
+    public static final int SWIPE_VELOCITY_THRESH_HOLD = 100;
+    public static final int LAYOUT_PADDING = 0;
+}
