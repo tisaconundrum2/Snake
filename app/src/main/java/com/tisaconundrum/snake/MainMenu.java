2cb419a3ed3b402821911f0c4a7c151ee52113ba
Nicholas Finch <ngf4@nau.edu>
2016-10-12 13:12:37 -0700

    modified:   app/src/main/java/com/tisaconundrum/snake/MainMenu.java deleted:    app/src/main/res/layout/activity_main.xml deleted:    app/src/main/res/layout/content_main.xml

--- app/src/main/java/com/tisaconundrum/snake/MainMenu.java
+++ app/src/main/java/com/tisaconundrum/snake/MainMenu.java
@@ -1,20 +1,22 @@
 package com.tisaconundrum.snake;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainMenu extends AppCompatActivity {
 
+    private RelativeLayout snakeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+        snakeLayout = (RelativeLayout) findViewById(R.id.snake_layout);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView
     }
+
 
 }
