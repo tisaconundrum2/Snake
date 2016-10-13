20704d46b51a57338b4d06e219db2fa9249bae37
Nicholas Finch <ngf4@nau.edu>
2016-10-12 22:33:37 -0700

    Adding files

---
+++ app/src/main/java/com/timbuchalka/snake/BombSnake.java
@@ -0,0 +1,14 @@
+package com.tisaconundrum.snake;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BombSnake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bomb_snake);
+    }
+
+}
