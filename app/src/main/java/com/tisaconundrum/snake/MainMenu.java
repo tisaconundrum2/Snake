c28d9ada3f755d5a18db0c4da6c5b04c739eff09
Nicholas Finch <ngf4@nau.edu>
2016-10-12 18:32:50 -0700

    Changes to be committed: 	modified:   app/src/main/java/com/tisaconundrum/snake/MainMenu.java 	modified:   app/src/main/res/layout/main_menu.xml

--- app/src/main/java/com/tisaconundrum/snake/MainMenu.java
+++ app/src/main/java/com/tisaconundrum/snake/MainMenu.java
@@ -1,20 +1,26 @@
-package com.tisaconundrum.snake;
+package com.timbuchalka.snake;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
+import com.tisaconundrum.snake.GameSettings;
+import com.tisaconundrum.snake.R;
 
 public class MainMenu extends AppCompatActivity {
 
     private RelativeLayout snakeLayout;
     private Animation compileAnim;
-    private AdView adView;
+    private ImageView classicBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +31,70 @@ public class MainMenu extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        adView = new adView(this);
-        adView.setAdSize(AdSize.SMART_BANNER);
-        adView.setAdUnitId(GameSettings.MY_AD_UNIT_ID);
-        snakeLayout.addView(adView);
-
-        //Test ad to the main page
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_TO_EMULATOR).build();
-        adView.loadAd(adRequest);
     }
 
 
+    private void initClassic() {
+        classicBtn = (ImageView) findViewById(R.id.classic);
+        compileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.anim_for_classic_button);
+        compileAnim.setDuration(GameSettings.ANIMATION_OPEN_BUTTON_DURATION);
+        compileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                classicBtn.setImageResource(R.mipmap.classic);
+                classicBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intentClassic = new Intent(MainMenu.this, ClassicSnake.class);
+                        intentClassic.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intentClassic);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        classicBtn.startAnimation(compileAnim);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
