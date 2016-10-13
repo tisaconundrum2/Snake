fec06425efe857492edab260a185a91d99131d5b
Nicholas Finch <ngf4@nau.edu>
2016-10-12 18:02:36 -0700

    modified:   app/src/main/java/com/tisaconundrum/snake/MainMenu.java Setting up ad information... Not really required

--- app/src/main/java/com/tisaconundrum/snake/MainMenu.java
+++ app/src/main/java/com/tisaconundrum/snake/MainMenu.java
@@ -3,19 +3,39 @@ package com.tisaconundrum.snake;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainMenu extends AppCompatActivity {
 
     private RelativeLayout snakeLayout;
+    private Animation compileAnim;
+    private AdView adView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
         snakeLayout = (RelativeLayout) findViewById(R.id.snake_layout);
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView
+        decorView.setSystemUiVisibility(uiOptions);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
+        adView = new adView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId(GameSettings.MY_AD_UNIT_ID);
+        snakeLayout.addView(adView);
+
+        //Test ad
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_TO_EMULATOR).build();
     }
 
 
